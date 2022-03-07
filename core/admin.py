edce47cc24a65b183e580fe2d765d75b9328b89d
Wanderer2436 <timka2436@gmail.com>
2022-03-07 13:08:54 +0500

    #4 Добавлена модель кураторов

--- core/admin.py
+++ core/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 import core.models
 
 admin.site.register(core.models.Students)
+admin.site.register(core.models.Curator)
--- core/models.py
+++ core/models.py
@@ -1,11 +1,34 @@
 from django.db import models
 
 
+class Curator(models.Model):
+    first_name = models.CharField('Имя', max_length=15)
+    second_name = models.CharField('Фамилия', max_length=15)
+
+    class Meta:
+        ordering = ['-first_name']
+        verbose_name = 'Куратор'
+        verbose_name_plural = 'Кураторы'
+
+    def __str__(self):
+        return self.second_name
+
+
+
+
 class Students(models.Model):
     first_name = models.CharField('Имя', max_length=15)
     second_name = models.CharField('Фамилия', max_length=15)
     cource_num = models.IntegerField('Курс')
     direction = models.CharField('Направление', max_length=15)
+    curator = models.ForeignKey('core.Curator', on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        ordering = ['-first_name']
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
 
     def __str__(self):
         return self.second_name
+
+
